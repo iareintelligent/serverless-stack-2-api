3d3be66df7153159a9f112962c844aaf4046fb2a
Topher <topher@bythecode.agency>
2018-11-09 18:24:24 -0800

    Used process.env variables for our functions tableNames

--- create.js
+++ create.js
@@ -3,22 +3,23 @@ import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
-  const data = JSON.parse(event.body);
-  const params = {
-    TableName: "notes",
-    Item: {
-      userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: uuid.v1(),
-      content: data.content,
-      attachment: data.attachment,
-      createdAt: Date.now()
-    }
-  };
+    const data = JSON.parse(event.body);
+    const params = {
+        // all functions have access to process.env from the serverless.yml environment
+        TableName: process.env.tableName,
+        Item: {
+            userId: event.requestContext.identity.cognitoIdentityId,
+            noteId: uuid.v1(),
+            content: data.content,
+            attachment: data.attachment,
+            createdAt: Date.now()
+        }
+    };
 
-  try {
-    await dynamoDbLib.call("put", params);
-    return success(params.Item);
-  } catch (e) {
-    return failure({ status: false });
-  }
+    try {
+        await dynamoDbLib.call("put", params);
+        return success(params.Item);
+    } catch (e) {
+        return failure({ status: false });
+    }
 }
